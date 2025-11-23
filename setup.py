69d8a6e195c90b5edf61b358cb10381fa719477a
Aryan <au3162233@gmail.com>
2025-11-23 10:57:10 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A machine learning project that predicts estimated food delivery time based on real-world factors such as restaurant location, delivery distance, order volume, traffic conditions, and more.',
+    author='Aryan',
+    license='',
+)
